6e306f05e361f9a32329f6511b9898efa2843dce
HOME <santiagolopezramirez275@gmail.com>
2022-10-22 22:12:15 -0500

    registro funcional

---
+++ excepciones.py
@@ -0,0 +1,23 @@
+class CineError(Exception):
+    def __init__(self, mensaje):
+        self.mensaje = mensaje
+
+
+class CuentaExistenteError(CineError):
+    def __init__(self, mensaje):
+        super().__init__(mensaje)
+
+
+class CuentaNoExistenteError(CineError):
+    def __init__(self, mensaje):
+        super().__init__(mensaje)
+
+
+class ContrasenaInvalida(CineError):
+    def __init__(self, mensaje):
+        super().__init__(mensaje)
+
+
+class EspaciosSinRellenar(CineError):
+    def __init__(self, mensaje):
+        super().__init__(mensaje)
